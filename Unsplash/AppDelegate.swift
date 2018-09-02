8881b6f5226ccdf5b7f3c148304e00050a8a7fbf
Michail Zarmakoupis <zarmakoupism@gmail.com>
2018-09-02 17:26:53 +0300

    Initial commit

---
+++ Unsplash/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Unsplash
+//
+//  Created by Michail Zarmakoupis on 02/09/2018.
+//  Copyright © 2018 Michail Zarmakoupis. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
