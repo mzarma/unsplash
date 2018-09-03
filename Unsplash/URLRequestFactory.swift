f17457836c710f80bce516406b88683ca17693bb
Michail Zarmakoupis <zarmakoupism@gmail.com>
2018-09-03 16:23:55 +0300

    Add URLRequestFactory with search request

---
+++ Unsplash/URLRequestFactory.swift
@@ -0,0 +1,32 @@
+//
+//  URLRequestFactory.swift
+//  Unsplash
+//
+//  Created by Michail Zarmakoupis on 03/09/2018.
+//  Copyright © 2018 Michail Zarmakoupis. All rights reserved.
+//
+
+import Foundation
+
+struct SearchParameters {
+    let page: Int
+    let term: String
+}
+
+final class URLRequestFactory {
+    private static let baseURLString = "https://api.unsplash.com"
+    private static let searchPhotosPath = "/search/photos"
+    private static let accessKey = NSDictionary(contentsOfFile: Bundle.main.path(forResource: "Info", ofType: "plist")!)!["AccessKey"] as! String
+        
+    static func search(parameters: SearchParameters) -> URLRequest {
+        var components = URLComponents(string: baseURLString)!
+        components.path = searchPhotosPath
+        
+        let page = URLQueryItem(name: "page", value: String(parameters.page))
+        let term =  URLQueryItem(name: "query", value: parameters.term)
+        let key = URLQueryItem(name: "client_id", value: accessKey)
+        components.queryItems = [page, term, key]
+        
+        return URLRequest(url: components.url!)
+    }
+}
---
+++ UnsplashTests/URLRequestFactoryTest.swift
@@ -0,0 +1,53 @@
+//
+//  URLRequestFactoryTest.swift
+//  UnsplashTests
+//
+//  Created by Michail Zarmakoupis on 03/09/2018.
+//  Copyright © 2018 Michail Zarmakoupis. All rights reserved.
+//
+
+import XCTest
+@testable import Unsplash
+
+class URLRequestFactoryTest: XCTestCase {
+    
+    func test_createsSearchURLRequest_withGetHTTPMethod() {
+        let request = SUT.search(parameters: params())
+
+        XCTAssertEqual(request.httpMethod, "GET")
+    }
+    
+    func test_createsSearchURLRequest_withCorrectPath() {
+        let request = SUT.search(parameters: params())
+
+        XCTAssertEqual(request.url?.scheme, "https")
+        XCTAssertEqual(request.url?.host, "api.unsplash.com")
+        XCTAssertEqual(request.url?.path, "/search/photos")
+    }
+    
+    func test_createsSearchURLRequest_withCorrectQueryItems() {
+        let params1 = params(page: 4, term: "a term")
+        let request1 = SUT.search(parameters: params1)
+        
+        let params2 = params(page: 2, term: "anotherTerm")
+        let request2 = SUT.search(parameters: params2)
+        
+        XCTAssertEqual(request1.url?.query, "page=4&query=a%20term&client_id=\(accessKey)")
+        XCTAssertEqual(request2.url?.query, "page=2&query=anotherTerm&client_id=\(accessKey)")
+    }
+    
+    // MARK: Helpers
+    
+    private typealias SUT = URLRequestFactory
+
+    private func params(page: Int = 1, term: String = "term") -> SearchParameters {
+        return SearchParameters(page: page, term: term)
+    }
+    
+    private var accessKey: String {
+        return NSDictionary(
+            contentsOfFile: Bundle
+                .main
+                .path(forResource: "Info", ofType: "plist")!)!["AccessKey"] as! String
+    }
+}
