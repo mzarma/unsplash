d296f73990681a8a421ee6808b507ae7c4b64c2c
Michail Zarmakoupis <zarmakoupism@gmail.com>
2018-09-05 17:22:04 +0300

    Add RemoteSearchResultFetcher

--- Unsplash/RemoteSearchResultFetcher.swift
+++ Unsplash/RemoteSearchResultFetcher.swift
@@ -8,35 +8,88 @@
 
 import Foundation
 
-struct Photo: Codable {
+struct Photo: Equatable, Codable {
     let identifier: String
-    let dateCreated: Date
+    let dateCreatedString: String
     let width: Int
     let height: Int
     let colorString: String
     let description: String
     let creator: Creator
-    let regularImageURLString: String
-    let smallImageURLString: String
-    let thumbnailImageURLString: String
-    let downloadImageLink: String
+    let imageURLs: ImageURLs
+    let imageLinks: ImageLinks
     
-    struct Creator: Codable {
+    enum CodingKeys: String, CodingKey {
+        case identifier = "id"
+        case dateCreatedString = "created_at"
+        case width = "width"
+        case height = "height"
+        case colorString = "color"
+        case description = "description"
+        case creator = "user"
+        case imageURLs = "urls"
+        case imageLinks = "links"
+    }
+    
+    struct Creator: Equatable, Codable {
         let identifier: String
         let username: String
         let name: String
-        let portfolioURLString: String
-        let smallProfileImageURLString: String
-        let mediumProfileImageURLString: String
-        let largeProfileImageURLString: String
-        let creatorPhotosURLString: String
+        let portfolioURLString: String?
+        let imageURLs: ImageURLs
+
+        enum CodingKeys: String, CodingKey {
+            case identifier = "id"
+            case username = "username"
+            case name = "name"
+            case portfolioURLString = "portfolio_url"
+            case imageURLs = "profile_image"
+        }
+        
+        struct ImageURLs: Equatable, Codable {
+            let smallProfileImageURLString: String?
+            let mediumProfileImageURLString: String?
+            let largeProfileImageURLString: String?
+            
+            enum CodingKeys: String, CodingKey {
+                case smallProfileImageURLString = "small"
+                case mediumProfileImageURLString = "medium"
+                case largeProfileImageURLString = "large"
+            }
+        }
+    }
+    
+    struct ImageURLs: Equatable, Codable {
+        let regular: String
+        let small: String
+        let thumbnail: String
+        
+        enum CodingKeys: String, CodingKey {
+            case regular = "regular"
+            case small = "small"
+            case thumbnail = "thumb"
+        }
+    }
+    
+    struct ImageLinks: Equatable, Codable {
+        let download: String
+        
+        enum CodingKeys: String, CodingKey {
+            case download = "download"
+        }
     }
 }
 
-struct SearchResult: Codable {
+struct SearchResult: Equatable, Codable {
     let totalPhotos: Int
     let totalPages: Int
     let photos: [Photo]
+    
+    enum CodingKeys: String, CodingKey {
+        case totalPhotos = "total"
+        case totalPages = "total_pages"
+        case photos = "results"
+    }
 }
 
 enum RemoteSearchResultFetcherError {
@@ -70,13 +123,23 @@ final class RemoteSearchResultFetcher: SearchResultFetcher {
         client.execute(request) { result in
             switch result {
             case .success(let data):
-                do {
-                    _ = try JSONSerialization.jsonObject(with: data)
-                } catch {
+                if let searchResult = RemoteSearchResultMapper.map(data: data) {
+                    completion(.success(searchResult))
+                } else {
                     completion(.error(.mapping))
                 }
             case .error(_): completion(.error(.httpClient))
             }
+        }
+    }
+}
+
+final class RemoteSearchResultMapper {
+    static func map(data: Data) -> SearchResult? {
+        if let searchResult = try? JSONDecoder().decode(SearchResult.self, from: data) {
+            return searchResult
+        } else {
+            return nil
         }
     }
 }
--- UnsplashTests/RemoteSearchResultFetcherTest.swift
+++ UnsplashTests/RemoteSearchResultFetcherTest.swift
@@ -44,28 +44,57 @@ class RemoteSearchResultFetcherTest: XCTestCase {
         }
     }
     
-        func test_completesWithMappingError() {
-            let sut = makeSUT(request: mockRequest())
-            var expectedResult: RemoteSearchResultFetcherResult?
-            var callCount = 0
-            sut.fetch(request: mockRequest()) { result in
-                expectedResult = result
-                callCount += 1
-            }
-    
-            client.complete?(.success(invalidJSONData))
-    
-            switch expectedResult! {
-            case .error(let error): XCTAssertEqual(error, .mapping)
-            case .success(_): XCTFail("Should complete with error")
-            }
+    func test_completesWithMappingError() {
+        let sut = makeSUT(request: mockRequest())
+        var expectedResult: RemoteSearchResultFetcherResult?
+        var callCount = 0
+        sut.fetch(request: mockRequest()) { result in
+            expectedResult = result
+            callCount += 1
         }
+        
+        client.complete?(.success(invalidJSONData))
+        
+        switch expectedResult! {
+        case .error(let error): XCTAssertEqual(error, .mapping)
+        case .success(_): XCTFail("Should complete with error")
+        }
+    }
+    
+    func test_completesWithSearchResult() {
+        let sut = makeSUT(request: mockRequest())
+        var expectedResult: RemoteSearchResultFetcherResult?
+        var callCount = 0
+        sut.fetch(request: mockRequest()) { result in
+            expectedResult = result
+            callCount += 1
+        }
+        
+        client.complete?(.success(validJSONData()))
+        
+        let profileImageURLs = Photo.Creator.ImageURLs(smallProfileImageURLString: "small profile image url", mediumProfileImageURLString: "medium profile image url", largeProfileImageURLString: "large profile image url")
+        let creator = Photo.Creator(identifier: "Ul0QVz12Goo", username: "ugmonk", name: "Jeff Sheldon", portfolioURLString: "portfolio url", imageURLs: profileImageURLs)
+        let imageURLs = Photo.ImageURLs(regular: "regular image url", small: "small image url", thumbnail: "thumbnail image url")
+        let imageLinks = Photo.ImageLinks(download: "download link")
+        let photos = [Photo(identifier: "eOLpJytrbsQ", dateCreatedString: "2014-11-18T14:35:36-05:00", width: 4000, height: 3000, colorString: "#A7A2A1", description: "A man drinking a coffee.", creator: creator, imageURLs: imageURLs, imageLinks: imageLinks)]
+        let expectedSearchResult = SearchResult(totalPhotos: 133, totalPages: 7, photos: photos)
+
+        
+        switch expectedResult! {
+        case .error(_): XCTFail("Should succeed with search result")
+        case .success(let searchResult): XCTAssertEqual(searchResult, expectedSearchResult)
+        }
+    }
 
     // MARK: Helpers
     
     private let client = HTTPClientStub()
     
     private let invalidJSONData = "invalid data".data(using: .utf8)!
+    
+    private func validJSONData() -> Data {
+        return try! JSONSerialization.data(withJSONObject: validJSON)
+    }
     
     private func makeSUT(request: URLRequest) -> RemoteSearchResultFetcher {
         let sut = RemoteSearchResultFetcher(client: client)
@@ -94,4 +123,56 @@ class RemoteSearchResultFetcherTest: XCTestCase {
             complete = completion
         }
     }
+    
+    private let validJSON: [String: Any] =
+        [
+            "total": 133,
+            "total_pages": 7,
+            "results": [
+                [
+                    "id": "eOLpJytrbsQ",
+                    "created_at": "2014-11-18T14:35:36-05:00",
+                    "width": 4000,
+                    "height": 3000,
+                    "color": "#A7A2A1",
+                    "likes": 286,
+                    "liked_by_user": false,
+                    "description": "A man drinking a coffee.",
+                    "user": [
+                        "id": "Ul0QVz12Goo",
+                        "username": "ugmonk",
+                        "name": "Jeff Sheldon",
+                        "first_name": "",
+                        "last_name": "",
+                        "instagram_username": "",
+                        "twitter_username": "",
+                        "portfolio_url": "portfolio url",
+                        "profile_image": [
+                            "small": "small profile image url",
+                            "medium": "medium profile image url",
+                            "large": "large profile image url"
+                        ],
+                        "links": [
+                            "self": "",
+                            "html": "",
+                            "photos": "",
+                            "likes": ""
+                        ]
+                    ],
+                    "current_user_collections": [],
+                    "urls": [
+                        "raw": "",
+                        "full": "",
+                        "regular": "regular image url",
+                        "small": "small image url",
+                        "thumb": "thumbnail image url"
+                    ],
+                    "links": [
+                        "self": "",
+                        "html": "",
+                        "download": "download link"
+                    ]
+                ]
+            ]
+    ]
 }
