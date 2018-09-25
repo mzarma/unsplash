be2d72ab095f2937d3a55e8f68b6fb53d10d5a66
Michail Zarmakoupis <zarmakoupism@gmail.com>
2018-09-25 15:05:57 +0300

    Add PresentablePhoto struct that contains thumbnailImage needed for PhotoListViewController

---
+++ Unsplash/Presentation/PresentablePhoto.swift
@@ -0,0 +1,30 @@
+//
+//  PresentablePhoto.swift
+//  Unsplash
+//
+//  Created by Michail Zarmakoupis on 25/09/2018.
+//  Copyright © 2018 Michail Zarmakoupis. All rights reserved.
+//
+
+import UIKit
+
+struct PresentablePhoto {
+    let identifier: String
+    let dateCreated: String
+    let width: Int
+    let height: Int
+    let colorString: String
+    let description: String
+    let creatorIdentifier: String
+    let creatorUsername: String
+    let creatorName: String
+    let creatorPortfolioURLString: String?
+    let creatorSmallProfileImageURLString: String?
+    let creatorMediumProfileImageURLString: String?
+    let creatorLargeProfileImageURLString: String?
+    let regularImageURLString: String
+    let smallImageURLString: String
+    let thumbnailImageURLString: String
+    let downloadImageLink: String
+    let thumbnailImage: UIImage
+}
