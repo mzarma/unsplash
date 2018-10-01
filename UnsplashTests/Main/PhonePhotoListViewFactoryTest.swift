f13e867a77c55ec149a145c3efcb2f63239295a5
Michail Zarmakoupis <zarmakoupism@gmail.com>
2018-10-01 19:05:50 +0300

    Refactor tests

--- UnsplashTests/Main/PhonePhotoListViewFactoryTest.swift
+++ UnsplashTests/Main/PhonePhotoListViewFactoryTest.swift
@@ -18,89 +18,64 @@ class PhonePhotoListViewFactoryTest: XCTestCase {
         super.tearDown()
     }
     
-    func test_containerHasChildSearchView() {
-        let sut = makeSUT()
-        
-        let containerViewController = sut.makePhotoListView { _ in }
-
-        XCTAssertTrue(sut.searchViewController.parent === containerViewController)
-        XCTAssertTrue(sut.searchViewController.view.superview === containerViewController.view)
-    }
-    
-    func test_containerHasChildPhotoListView() {
-        let sut = makeSUT()
-        
-        let containerViewController = sut.makePhotoListView { _ in }
-        
-        XCTAssertTrue(sut.listViewController.parent === containerViewController)
-        XCTAssertTrue(sut.listViewController.view.superview === containerViewController.view)
-    }
-    
     func test_photoListView_startsWithNoPhotoText() {
-        let sut = makeSUT()
-        _ = sut.makePhotoListView { _ in }
-        XCTAssertEqual(sut.listViewController.noPhotoCell().text, "No photos")
+        XCTAssertEqual(makeSUT().photoListView.noPhotoCell().text, "No photos")
     }
     
     func test_searchResultFetcherFetches_whenSearchedIsFired() {
         let expectedSearchParameters = SearchParameters(page: 1, term: "a term")
         let sut = makeSUT()
-        _ = sut.makePhotoListView { _ in }
-        
+
         XCTAssertEqual(searchResultFetcher.fetchCallCount, 0)
         XCTAssertEqual(searchResultFetcher.requests, [])
-        
-        sut.searchViewController.clickSearchButton(with: "a term")
-        
+
+        sut.searchView.clickSearchButton(with: "a term")
+
         XCTAssertEqual(searchResultFetcher.fetchCallCount, 1)
         XCTAssertEqual(searchResultFetcher.requests, [URLRequestFactory.search(parameters: expectedSearchParameters)])
     }
-    
+
     func test_photoListView_showsNoPhotoCell_whenSearchResultFetchingFails() {
         let sut = makeSUT()
-        _ = sut.makePhotoListView { _ in }
 
-        sut.searchViewController.clickSearchButton(with: "a term")
+        sut.searchView.clickSearchButton(with: "a term")
         searchResultFetcher.complete?(.error(.remote))
-        
+
         XCTAssertEqual(searchResultFetcher.fetchCallCount, 1)
-        XCTAssertEqual(sut.listViewController.numberOfItems(), 1)
-        XCTAssertEqual(sut.listViewController.noPhotoCell().text, "No photos")
+        XCTAssertEqual(sut.photoListView.numberOfItems(), 1)
+        XCTAssertEqual(sut.photoListView.noPhotoCell().text, "No photos")
     }
-    
+
     func test_photoListView_showsNoPhotoCell_whenSearchResultFetcherCompletesWithZeroPhotos() {
         let sut = makeSUT()
-        _ = sut.makePhotoListView { _ in }
-        
-        sut.searchViewController.clickSearchButton(with: "a term")
+
+        sut.searchView.clickSearchButton(with: "a term")
         let searchResult = CoreSearchResult(totalPhotos: 0, totalPages: 1, photos: [])
         searchResultFetcher.complete?(.success(searchResult))
-        
+
         XCTAssertEqual(searchResultFetcher.fetchCallCount, 1)
-        XCTAssertEqual(sut.listViewController.numberOfItems(), 1)
-        XCTAssertEqual(sut.listViewController.noPhotoCell().text, "No photos")
+        XCTAssertEqual(sut.photoListView.numberOfItems(), 1)
+        XCTAssertEqual(sut.photoListView.noPhotoCell().text, "No photos")
     }
 
     func test_photoListView_photoCellWithNoImage_whenOnePhotoAndImageProviderReturnsNil() {
         let sut = makeSUT()
-        _ = sut.makePhotoListView { _ in }
 
-        sut.searchViewController.clickSearchButton(with: "a term")
+        sut.searchView.clickSearchButton(with: "a term")
         let photo = corePhoto(description: "a description")
         let searchResult = CoreSearchResult(totalPhotos: 1, totalPages: 1, photos: [photo])
         searchResultFetcher.complete?(.success(searchResult))
 
         XCTAssertEqual(searchResultFetcher.fetchCallCount, 1)
-        XCTAssertEqual(sut.listViewController.numberOfItems(), 1)
-        XCTAssertEqual(sut.listViewController.photoCell(for: 0).text, "a description")
-        XCTAssertNil(sut.listViewController.photoCell(for: 0).photoImage)
+        XCTAssertEqual(sut.photoListView.numberOfItems(), 1)
+        XCTAssertEqual(sut.photoListView.photoCell(for: 0).text, "a description")
+        XCTAssertNil(sut.photoListView.photoCell(for: 0).photoImage)
     }
-    
+
     func test_photoListView_showsPhotoCell_withOnePhoto() {
         let sut = makeSUT()
-        _ = sut.makePhotoListView { _ in }
 
-        sut.searchViewController.clickSearchButton(with: "a term")
+        sut.searchView.clickSearchButton(with: "a term")
         let photo = corePhoto(identifier: "an identifier", description: "a description")
         let searchResult = CoreSearchResult(totalPhotos: 1, totalPages: 1, photos: [photo])
         searchResultFetcher.complete?(.success(searchResult))
@@ -108,83 +83,80 @@ class PhonePhotoListViewFactoryTest: XCTestCase {
         let image = testImage()
         imageProvider.imagesByIdentifier = ["an identifier": image]
 
-        XCTAssertEqual(sut.listViewController.numberOfItems(), 1)
-        XCTAssertEqual(sut.listViewController.photoCell(for: 0).text, "a description")
-        XCTAssertEqual(sut.listViewController.photoCell(for: 0).photoImage, image)
+        XCTAssertEqual(sut.photoListView.numberOfItems(), 1)
+        XCTAssertEqual(sut.photoListView.photoCell(for: 0).text, "a description")
+        XCTAssertEqual(sut.photoListView.photoCell(for: 0).photoImage, image)
     }
-    
+
     func test_photoListView_showsPhotoCells() {
         let sut = makeSUT()
-        _ = sut.makePhotoListView { _ in }
-        
-        sut.searchViewController.clickSearchButton(with: "a term")
+
+        sut.searchView.clickSearchButton(with: "a term")
         let photo1 = corePhoto(identifier: "identifier1", description: "description1")
         let photo2 = corePhoto(identifier: "identifier2", description: "description2")
         let photo3 = corePhoto(identifier: "identifier3", description: "description3")
-        
+
         let searchResult = CoreSearchResult(totalPhotos: 3, totalPages: 1, photos: [photo1, photo2, photo3])
         searchResultFetcher.complete?(.success(searchResult))
-        
+
         let image1 = testImage(width: 100, height: 100)
         let image2 = testImage(width: 200, height: 200)
-        
-        imageProvider.imagesByIdentifier = ["identifier1": image1, "identifier2": image2]
-        
-        XCTAssertEqual(sut.listViewController.numberOfItems(), 3)
-        XCTAssertEqual(sut.listViewController.photoCell(for: 0).text, "description1")
-        XCTAssertEqual(sut.listViewController.photoCell(for: 1).text, "description2")
-        XCTAssertEqual(sut.listViewController.photoCell(for: 2).text, "description3")
 
-        XCTAssertEqual(sut.listViewController.photoCell(for: 0).photoImage, image1)
-        XCTAssertEqual(sut.listViewController.photoCell(for: 1).photoImage, image2)
-        XCTAssertNil(sut.listViewController.photoCell(for: 2).photoImage)
+        imageProvider.imagesByIdentifier = ["identifier1": image1, "identifier2": image2]
+
+        XCTAssertEqual(sut.photoListView.numberOfItems(), 3)
+        XCTAssertEqual(sut.photoListView.photoCell(for: 0).text, "description1")
+        XCTAssertEqual(sut.photoListView.photoCell(for: 1).text, "description2")
+        XCTAssertEqual(sut.photoListView.photoCell(for: 2).text, "description3")
+
+        XCTAssertEqual(sut.photoListView.photoCell(for: 0).photoImage, image1)
+        XCTAssertEqual(sut.photoListView.photoCell(for: 1).photoImage, image2)
+        XCTAssertNil(sut.photoListView.photoCell(for: 2).photoImage)
     }
 
     func test_photoListView_doesNotDelegateSelection_whenNoPhotoCellIsSelected() {
         var photoSelectionCount = 0
         var selectedPhoto: CorePhoto?
-        
-        let sut = makeSUT()
-        _ = sut.makePhotoListView { photo in
+
+        let sut = makeSUT() { photo in
             photoSelectionCount += 1
             selectedPhoto = photo
         }
-        
-        sut.searchViewController.clickSearchButton(with: "a term")
+
+        sut.searchView.clickSearchButton(with: "a term")
         let searchResult = CoreSearchResult(totalPhotos: 0, totalPages: 1, photos: [])
         searchResultFetcher.complete?(.success(searchResult))
-        
+
         XCTAssertEqual(searchResultFetcher.fetchCallCount, 1)
-        XCTAssertEqual(sut.listViewController.numberOfItems(), 1)
-        XCTAssertEqual(sut.listViewController.noPhotoCell().text, "No photos")
-        
-        sut.listViewController.selectItem(0)
-        
+        XCTAssertEqual(sut.photoListView.numberOfItems(), 1)
+        XCTAssertEqual(sut.photoListView.noPhotoCell().text, "No photos")
+
+        sut.photoListView.selectItem(0)
+
         XCTAssertEqual(photoSelectionCount, 0)
         XCTAssertNil(selectedPhoto)
     }
-    
+
     func test_photoListView_delegatesSelection_whenPhotoCellIsSelected() {
         var photoSelectionCount = 0
         var selectedPhoto: CorePhoto?
 
-        let sut = makeSUT()
-        _ = sut.makePhotoListView { photo in
+        let sut = makeSUT() { photo in
             photoSelectionCount += 1
             selectedPhoto = photo
         }
 
-        sut.searchViewController.clickSearchButton(with: "a term")
+        sut.searchView.clickSearchButton(with: "a term")
         let photo = corePhoto(identifier: "an identifier", description: "a description")
         let searchResult = CoreSearchResult(totalPhotos: 1, totalPages: 1, photos: [photo])
         searchResultFetcher.complete?(.success(searchResult))
-        
-        XCTAssertEqual(sut.listViewController.numberOfItems(), 1)
-        XCTAssertEqual(sut.listViewController.photoCell(for: 0).text, "a description")
-        XCTAssertNil(sut.listViewController.photoCell(for: 0).photoImage)
-        
-        sut.listViewController.selectItem(0)
-        
+
+        XCTAssertEqual(sut.photoListView.numberOfItems(), 1)
+        XCTAssertEqual(sut.photoListView.photoCell(for: 0).text, "a description")
+        XCTAssertNil(sut.photoListView.photoCell(for: 0).photoImage)
+
+        sut.photoListView.selectItem(0)
+
         XCTAssertEqual(photoSelectionCount, 1)
         XCTAssertEqual(selectedPhoto, photo)
     }
@@ -194,11 +166,16 @@ class PhonePhotoListViewFactoryTest: XCTestCase {
     private let searchResultFetcher = SearchResultFetcherSpy()
     private let imageProvider = ImageProviderStub()
     private typealias SUT = PhonePhotoListViewFactory<SearchResultFetcherSpy>
+    private typealias Search = SearchViewController
+    private typealias PhotoList = PhotoListViewController
     
-    private func makeSUT() -> SUT {
+    private func makeSUT(_ selected: @escaping (CorePhoto) -> Void = { _ in }) -> (SUT, searchView: Search, photoListView: PhotoList) {
         let sut = PhonePhotoListViewFactory(searchResultFetcher, imageProvider)
         weakSUT = sut
-        return sut
+        let container = sut.makePhotoListView(selected)
+        let searchView = container.children.filter { $0 is Search }.first as! Search
+        let photoListView = container.children.filter { $0 is PhotoList }.first as! PhotoList
+        return (sut, searchView, photoListView)
     }
     
     private class SearchResultFetcherSpy: SearchResultFetcher {
